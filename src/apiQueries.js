3b7c266f5021a1e6dfc70d416e66074cfb6bc5e8
dan branton <daniel.branton@uptimesoftware.com>
2014-06-06 16:40:31 -0400

    Add in api queries from element-status-gadget

--- src/apiQueries.js
+++ src/apiQueries.js
@@ -12,6 +12,47 @@ function apiQueries() {
         return deferred.promise;
     }
 
+    function getElements(elementId) {
+        var deferred = UPTIME.pub.gadgets.promises.defer();
+        $.ajax("/api/v1/elements/" + elementId, {
+            cache : false
+        }).done(function(data, textStatus, jqXHR) {
+            deferred.resolve(data);
+        }).fail(function(jqXHR, textStatus, errorThrown) {
+            deferred.reject(UPTIME.pub.errors.toDisplayableJQueryAjaxError(jqXHR, textStatus, errorThrown, this));
+        });
+        return deferred.promise;
+
+    }
+
+        this.getElementStatus = function(elementId) {
+        var deferred = UPTIME.pub.gadgets.promises.defer();
+        $.ajax("/api/v1/elements/" + elementId + "/status", {
+            cache : false
+        }).done(function(data, textStatus, jqXHR) {
+            deferred.resolve(data);
+        }).fail(function(jqXHR, textStatus, errorThrown) {
+            deferred.reject(UPTIME.pub.errors.toDisplayableJQueryAjaxError(jqXHR, textStatus, errorThrown, this));
+        });
+        return deferred.promise;
+    };
+
+    this.getAllElements = function() {
+        var deferred = UPTIME.pub.gadgets.promises.defer();
+        $.ajax("/api/v1/elements", {
+            cache : false
+        }).done(function(data, textStatus, jqXHR) {
+            if (data.length == 0) {
+                deferred.reject("No elements found.");
+            } else {
+                deferred.resolve(data);
+            }
+        }).fail(function(jqXHR, textStatus, errorThrown) {
+            deferred.reject(UPTIME.pub.errors.toDisplayableJQueryAjaxError(jqXHR, textStatus, errorThrown, this));
+        });
+        return deferred.promise;
+    };
+
     function createGroupFilter(groupIds) {
         var deferred = UPTIME.pub.gadgets.promises.defer();
         $.ajax("/api/v1/groups/filter", {
